15154cddb2228aa006c8aeb89efc1881673cd30b
huynhquynh-dev <huynhquynh-dev>
2021-03-15 16:18:25 +0700

    First project

---
+++ src/main/java/com/lionhuynh/springframework5/SpringFramework5Application.java
@@ -0,0 +1,13 @@
+package com.lionhuynh.springframework5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramework5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFramework5Application.class, args);
+    }
+
+}
