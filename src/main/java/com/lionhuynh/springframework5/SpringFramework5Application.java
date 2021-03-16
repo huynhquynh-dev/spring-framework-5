fa5e69f32e96c1834439bcc3d8fc419b36535bdd
huynhquynh-dev <huynhquynh-dev>
2021-03-16 10:58:03 +0700

    Added service and controller app

--- src/main/java/com/lionhuynh/springframework5/SpringFramework5Application.java
+++ src/main/java/com/lionhuynh/springframework5/SpringFramework5Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringFramework5Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SpringFramework5Application.class, args);
-
     }
 }
---
+++ src/main/java/com/lionhuynh/springframework5/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.lionhuynh.springframework5.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
