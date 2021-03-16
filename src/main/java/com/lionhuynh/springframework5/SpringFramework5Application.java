d476c437fae241d3a17806aa7f1138f3b4913237
huynhquynh-dev <huynhquynh-dev>
2021-03-16 14:41:25 +0700

    Added XML configuration

--- src/main/java/com/lionhuynh/springframework5/SpringFramework5Application.java
+++ src/main/java/com/lionhuynh/springframework5/SpringFramework5Application.java
@@ -1,12 +1,18 @@
 package com.lionhuynh.springframework5;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringFramework5Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringFramework5Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpringFramework5Application.class, args);
+        ChuckNorrisQuotes chuckNorrisQuotes = (ChuckNorrisQuotes) ctx.getBean("chuckNorrisQuotes");
+        System.out.println(chuckNorrisQuotes.getRandomQuote());
     }
 }
