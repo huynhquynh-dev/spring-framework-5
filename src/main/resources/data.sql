029d83930f8e20f5be23c2be538616add3212797
huynhquynh-dev <huynhquynh-dev>
2021-03-17 15:30:35 +0700

    Added data init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category(description) VALUES ('Example1');
+INSERT INTO category(description) VALUES ('Example2');
+INSERT INTO category(description) VALUES ('Example3');
+INSERT INTO category(description) VALUES ('Example4');
