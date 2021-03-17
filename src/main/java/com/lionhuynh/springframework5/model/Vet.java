ae5fced76224303957746a5da3e27db20752d2c5
huynhquynh-dev <huynhquynh-dev>
2021-03-17 22:51:35 +0700

    Added JPA Relationships

--- src/main/java/com/lionhuynh/springframework5/model/Vet.java
+++ src/main/java/com/lionhuynh/springframework5/model/Vet.java
@@ -1,9 +1,14 @@
 package com.lionhuynh.springframework5.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany
+    @JoinTable(name = "vets_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 }
