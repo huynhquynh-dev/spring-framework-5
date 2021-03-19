cf162d1cf837972531ce42198fc323d7b3935d44
huynhquynh-dev <huynhquynh-dev>
2021-03-19 15:18:57 +0700

    Added Mockito Argument Capture

--- src/test/java/com/lionhuynh/springframework5/controllers/RecipeControllerTest.java
+++ src/test/java/com/lionhuynh/springframework5/controllers/RecipeControllerTest.java
@@ -1,11 +1,16 @@
 package com.lionhuynh.springframework5.controllers;
 
+import com.lionhuynh.springframework5.model.Recipe;
 import com.lionhuynh.springframework5.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -31,11 +36,28 @@ class RecipeControllerTest {
     @Test
     void getAll() {
 
+//        Given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(4L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+//        when
         String viewName = recipeController.getAll(model);
 
+//        when
         assertEquals("recipe/index", viewName);
-
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setRecipeController = argumentCaptor.getValue();
+        assertEquals(2, setRecipeController.size());
     }
 }
