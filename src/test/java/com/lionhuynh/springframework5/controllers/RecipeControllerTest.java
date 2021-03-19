e19d53bf0e29d39a89fec4d8dbfa1392f1f1ec33
huynhquynh-dev <huynhquynh-dev>
2021-03-19 15:38:44 +0700

    Added Mockito: test spring MockMVC

--- src/test/java/com/lionhuynh/springframework5/controllers/RecipeControllerTest.java
+++ src/test/java/com/lionhuynh/springframework5/controllers/RecipeControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -14,6 +17,9 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class RecipeControllerTest {
 
@@ -31,6 +37,14 @@ class RecipeControllerTest {
         MockitoAnnotations.openMocks(this);
 
         recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+        mockMvc.perform(get("/recipe/all"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/index"));
     }
 
     @Test
